8185a47183c7924f0001d987ca9ed0b23042baaa
Sean Fischer <sean.fischer@asc.upenn.edu>
2018-07-10 14:54:14 -0400

    Update Function
    Function now requires noncensus and uses the included zip_codes data to check zips and fips

--- R/getProviders.R
+++ R/getProviders.R
@@ -4,21 +4,23 @@
 #' @param lat The latitude of the location of interest; optional if a zipcode is provided
 #' @param long The longitude of the location of interest; optional if a zipcode is provided
 #' @param zip The zipcode of the location of interest as a character string; an optional parameter when coordinates are not known
-#' @param fips The fips code of the location of interest as a numeric string with no leading zeros; an optional parameter when coordinates are not known
+#' @param fips The county-level fips code of the location of interest as a numeric string with no leading zeros; an optional parameter when coordinates are not known
 #' @param time The segment of data of interest; check the API documentation for options
 #' @return A data frame of mobile broadband providers in a given location along with relevant technology performance metrics.
 #' @import jsonlite
 #' @import tidyr
+#' @import noncensus
 #' @keywords mobile broadband, API
 #' @export
 #' @examples
 #' getProviders(lat = 39.958520, lon = -75.198857, time = "jun2014")
 #' getProviders(zip = "19104", time = "jun2014")
-#' getProviders(42101, time = "jun2014")
+#' getProviders(fips = 42101, time = "jun2014")
 
 getProviders <- function(lat, lon, zip, fips, time) {
   require(jsonlite)
   require(tidyr)
+  require(noncensus)
   if(missing(time)) {
     error("You need to supply the time period of data you wish to consider.")
   }
@@ -28,12 +30,12 @@ getProviders <- function(lat, lon, zip, fips, time) {
         error("You need to supply either the latitude and longitude, zipcode, or fips code.")
       }
       else {
-        lat <- countyFips[which(countyFips$FIPS == fips)]
-        lon <- countyFips[which(countyFips$FIPS == fips)]
+        lat <- zip_codes$latitude[which(zip_codes$fips == fips)]
+        lon <- zip_codes$longitude[which(zip_codes$fips == fips)]
       }
     } else {
-      lat <- zips$lat[which(zips$zip == zip)]
-      lon <- zips$lon[which(zips$zip == zip)]
+      lat <- zip_codes$latitude[which(zip_codes$zip == zip)]
+      lon <- zip_codes$longitude[which(zip_codes$zip == zip)]
     }
   }
   url <- paste("https://www.broadbandmap.gov/broadbandmap/broadband/",
