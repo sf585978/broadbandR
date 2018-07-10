6e0a6d2fe27308e38e9db39d82ea39f93508963b
Sean Fischer <sean.fischer@asc.upenn.edu>
2018-07-10 09:03:49 -0400

    documentation update
    Updated documentaiton for getProviders to indicate which arguments are optional and that zip code must be provided as a string

--- R/getProviders.R
+++ R/getProviders.R
@@ -1,9 +1,9 @@
 #' A function to retrieve mobile broadband provider information
 #' 
 #' A function for interfacing with the national broadband map API.
-#' @param lat The latitude of the location of interest
-#' @param long The longitude of the location of interest
-#' @param zip The zipcode of the location of interest; an optional parameter when coordinates are not known
+#' @param lat The latitude of the location of interest; optional if a zipcode is provided
+#' @param long The longitude of the location of interest; optional if a zipcode is provided
+#' @param zip The zipcode of the location of interest as a character string; an optional parameter when coordinates are not known
 #' @param time The segment of data of interest; check the API documentation for options
 #' @return A data frame of mobile broadband providers in a given location along with relevant technology performance metrics.
 #' @keywords mobile broadband, API
@@ -13,14 +13,17 @@
 #' getProviders("19104", "jun2014")
 
 getProviders <- function(lat, lon, zip, time) {
-  require(readr)
   require(jsonlite)
   require(tidyr)
+  if(missing(time)) {
+    error("You need to supply the time period of data you wish to consider.")
+  }
   if (missing(lat) & missing(lon)) {
     if(missing(zip)) {
       error("You need to supply either the latitude and longitude or a zip code.")
     } else {
-      
+      lat <- zips$lat[which(zips$zip == zip)]
+      lon <- zips$lon[which(zips$zip == zip)]
     }
   }
   url <- paste("https://www.broadbandmap.gov/broadbandmap/broadband/",
@@ -33,9 +36,9 @@ getProviders <- function(lat, lon, zip, time) {
                sep = "")
   out <- fromJSON(url)
   if (test$status == "OK") {
-    cat("Providers pulled successfully.")
     out2 <- unnest(out$Results$wirelessServices)
     colnames(out2)[7:22] <- paste(colnames(out2)[7:22], time, sep = "_")
+    cat("Providers pulled successfully.")
     return(out2)
   } else {
     error("Providers not pulled successfully.")
