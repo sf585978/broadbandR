502f0eac004d302cbd200be3d84fe3c396d13f7b
Sean Fischer <sean.fischer@asc.upenn.edu>
2018-07-10 09:07:46 -0400

    new documentation
    Added documentation for U.S. zip code to lat/lon data frame

---
+++ R/zips.R
@@ -0,0 +1,13 @@
+#' Latitude and longitude for U.S. zipcodes
+#' 
+#' A dataset containing the latitude and longitude coordinates for U.S. 
+#' zipcodes.
+#' 
+#' @format A data frame with 33144 rows and 3 variables:
+#' \describe{
+#' \item{zip}{zipcode, as a character}
+#' \item{lat}{latitude, as a numeric}
+#' \item{lon}{longitude, as a numeric}
+#' }
+#' @source \url{https://gist.github.com/erichurst/7882666}
+"zips"
