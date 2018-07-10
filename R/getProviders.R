99366878af8bd4603e38ed4306f8658d86dcdab0
Sean Fischer <sean.fischer@asc.upenn.edu>
2018-07-10 08:58:37 -0400

    Initial commit

---
+++ R/getProviders.R
@@ -0,0 +1,43 @@
+#' A function to retrieve mobile broadband provider information
+#' 
+#' A function for interfacing with the national broadband map API.
+#' @param lat The latitude of the location of interest
+#' @param long The longitude of the location of interest
+#' @param zip The zipcode of the location of interest; an optional parameter when coordinates are not known
+#' @param time The segment of data of interest; check the API documentation for options
+#' @return A data frame of mobile broadband providers in a given location along with relevant technology performance metrics.
+#' @keywords mobile broadband, API
+#' @export
+#' @examples
+#' getProviders(39.958520, -75.198857, "jun2014")
+#' getProviders("19104", "jun2014")
+
+getProviders <- function(lat, lon, zip, time) {
+  require(readr)
+  require(jsonlite)
+  require(tidyr)
+  if (missing(lat) & missing(lon)) {
+    if(missing(zip)) {
+      error("You need to supply either the latitude and longitude or a zip code.")
+    } else {
+      
+    }
+  }
+  url <- paste("https://www.broadbandmap.gov/broadbandmap/broadband/",
+               time,
+               "/wireless?latitude=",
+               lat,
+               "&longitude=",
+               lon,
+               "&maxResults=1000&format=json",
+               sep = "")
+  out <- fromJSON(url)
+  if (test$status == "OK") {
+    cat("Providers pulled successfully.")
+    out2 <- unnest(out$Results$wirelessServices)
+    colnames(out2)[7:22] <- paste(colnames(out2)[7:22], time, sep = "_")
+    return(out2)
+  } else {
+    error("Providers not pulled successfully.")
+  }
+}
